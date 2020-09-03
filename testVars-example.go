7b9d67780671ff84634aeca718ce897b263e4487
Ron Dyck <rond@webbtech.net>
2020-09-03 11:29:56 -0400

    - cleaned up tests

---
+++ testVars-example.go
@@ -0,0 +1,11 @@
+package auth
+
+// const (
+// 	cognitoClientID = "" // cognito client id
+// 	jwksURL         = "https://cognito-idp.{region}.amazonaws.com/{cognitoPoolID}/.well-known/jwks.json"
+// 	badJwksURL      = "https://cognito-idp.ca-central-1.amazonaws.com//.well-known/jwks.json"
+// 	username        = "" // expected username associated with token
+// 	invalidToken    = "" // invalid token using hmac or other than RSA encryption
+// 	expiredToken    = "" // other valid token that has expired
+// 	validToken      = "" // a valid token
+// )
